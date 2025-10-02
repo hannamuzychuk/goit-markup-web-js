b59d86c67e3130fbd5497a67b12945bedfbb9e62
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-10-02 22:22:10 +0200

    add hometask1

---
+++ js/task-1.js
@@ -0,0 +1,23 @@
+// const userAge = 15;
+// if (userAge >= 18) {
+//     console.log("Welcome");
+// } else {
+//     console.log("Goodbye");
+// }
+
+
+// const userBalance = 500;
+// if (userBalance > 1000) {
+//     console.log("Buy present for 1000");
+
+// } else if (userBalance > 800) {
+//     console.log("Buy present for 800");
+
+// } else {
+//     console.log("You cann't buy present");
+// }
+// function foo(i) {
+//     console.log(i);
+//     foo(i+=1)
+// }
+// foo(1)
