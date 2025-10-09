b90b7a980fb8cc6c1e5444c7f7599e9c1dadf610
Hanna Muzychuk <hannamuzychuk@gmail.com>
2025-10-09 10:20:35 +0200

    add task

---
+++ js/module-1.1.js
@@ -0,0 +1,46 @@
+// Task-1
+// Create two variables firstName and lastName.
+// Assign them your first and last name.
+// Combine these two strings into one, with a space between them. Write the result to the variable fullName.
+// Print fullName to the console.
+
+const firstName = "Hanna";
+const secondName = "Muzychuk";
+const fullName = `${firstName} ${secondName}`;
+console.log(fullName);
+
+// Task-2
+// Create two variables a and b, assign them some integers.
+// Perform addition, subtraction, multiplication and division on them.
+// Write the results to the variables sum, difference, product, quotient.
+// Print all these results to the console.
+
+const a = 25;
+const b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
+
+// Task-3
+// Create a variable radius and assign it some value (for example, 5).
+// Use the formula for the area of ​​a circle (π * r²) to calculate the area of ​​a circle with the radius you specified.
+// Use the value π = 3.14. Write the result to the variable area.
+// Print area to the console.
+
+const radius = 5;
+const p = 3.14;
+const area = (p * (radius ** 2));
+
+console.log(area);
+
+// Task-4
+// Create a variable someString and assign it any string.
+// Calculate the length of this string using the .length property.
+// Print the length of the string to the console.
+
+const someString = "Create a variable someString and assign it any string.";
+const stringLength = someString.length;
+console.log(stringLength);
